6feace772a86c1dc59536d180bce182caafbf601
antti.leppa@otavanopisto.fi <antti.leppa@otavanopisto.fi@1c9ff82f-59b1-6a62-74d4-2be32bec2a44>
2010-09-14 12:06:32 +0000

    ** Added new tool for database upgrading

---
+++ trunk/PyramusUpdater/src/fi/pyramus/updater/core/UpgradeBatch.java
@@ -0,0 +1,44 @@
+package fi.pyramus.updater.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.mapping.Column;
+import org.hibernate.mapping.ForeignKey;
+import org.hibernate.mapping.Table;
+
+public class UpgradeBatch {
+  
+  public UpgradeBatch() {
+  }
+  
+  public void addCreateTable(Table table) {
+    operations.add(new CreateTableUpdateOperation(table));
+  }
+  
+  public void addCreateForeignKey(ForeignKey foreignKey) {
+    operations.add(new CreateForeignKeyUpdateOperation(foreignKey));
+  }
+  
+  public void addAddColumn(Table table, Column column) {
+    operations.add(new AddColumnUpdateOperation(table, column));
+  }
+  
+  public void addDropColumn(Table table, String columnName) {
+    operations.add(new DropColumnUpdateOperation(table, columnName));
+  }
+ 
+  public void addDropForeignKey(Table table, String foreignKeyName) {
+    operations.add(new DropForeignKeyUpdateOperation(table, foreignKeyName));
+  }
+  
+  public void addDropTable(String tableName) {
+    operations.add(new DropTableUpdateOperation(tableName));
+  }
+  
+  public List<UpdateOperation> getOperations() {
+    return operations;
+  }
+
+  private List<UpdateOperation> operations = new ArrayList<UpdateOperation>();
+}
