830bb33b6fd56811f6ffd8b819b501b447937567
antti.leppa <antti.leppa@otavanopisto.fi>
2010-12-09 06:59:58 +0000

    ** Added courseNumber to createTransferCredit method in GradingDAO ** Renamed field name into courseName in TransferCreditEntity in WebServices ** Added courseNumber to TransferCreditEntity in WebServices ** Refactored GradingService createTransferCredit to use String instead of Boolean when determining optionality of course ** Added courseNumber to createTransferCredit method in WebServices

--- PyramusWebServices/src/fi/pyramus/services/entities/grading/TransferCreditEntityFactory.java
+++ PyramusWebServices/src/fi/pyramus/services/entities/grading/TransferCreditEntityFactory.java
@@ -1,7 +1,6 @@
 package fi.pyramus.services.entities.grading;
 
 import fi.pyramus.domainmodel.grading.TransferCredit;
-import fi.pyramus.persistence.usertypes.CourseOptionality;
 import fi.pyramus.services.entities.EntityFactory;
 
 public class TransferCreditEntityFactory implements EntityFactory<TransferCreditEntity> {
@@ -12,10 +11,8 @@ public class TransferCreditEntityFactory implements EntityFactory<TransferCredit
     
     TransferCredit transferCredit = (TransferCredit) domainObject;
     
-    Boolean optinal = transferCredit.getOptionality() != null ? transferCredit.getOptionality() == CourseOptionality.MANDATORY ? false : true : null;
-
     return new TransferCreditEntity(transferCredit.getId(), transferCredit.getStudent().getId(), transferCredit.getDate(), transferCredit.getGrade().getId(),
-        transferCredit.getVerbalAssessment(), transferCredit.getAssessingUser().getId(), transferCredit.getArchived(), transferCredit.getCourseName(), transferCredit
-            .getCourseLength().getUnits(), transferCredit.getCourseLength().getUnit().getId(), transferCredit.getSchool().getId(), transferCredit.getSubject().getId(), optinal);
+        transferCredit.getVerbalAssessment(), transferCredit.getAssessingUser().getId(), transferCredit.getArchived(), transferCredit.getCourseName(), transferCredit.getCourseNumber(), 
+        transferCredit.getCourseLength().getUnits(), transferCredit.getCourseLength().getUnit().getId(), transferCredit.getSchool().getId(), transferCredit.getSubject().getId(), transferCredit.getOptionality().toString());
   }
 }
