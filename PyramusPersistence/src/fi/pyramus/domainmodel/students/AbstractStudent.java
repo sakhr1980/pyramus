1a61cf03accd2d4d81bfc0aa8b92c0e66bfa1607
antti.leppa <antti.leppa@otavanopisto.fi>
2011-01-17 09:19:04 +0000

    ** Removed unnecessary imports from abstract student

--- PyramusPersistence/src/fi/pyramus/domainmodel/students/AbstractStudent.java
+++ PyramusPersistence/src/fi/pyramus/domainmodel/students/AbstractStudent.java
@@ -1,8 +1,6 @@
 package fi.pyramus.domainmodel.students;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
