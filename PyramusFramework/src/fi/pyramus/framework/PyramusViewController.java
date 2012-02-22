0403cc943b9cb26c8260ee0465930112d8077bf1
ainen <antti.viljakainen@otavanopisto.fi>
2012-02-22 08:01:40 +0000

    - fixes to get the version closer to "up and running"

--- PyramusFramework/src/fi/pyramus/framework/PyramusViewController.java
+++ PyramusFramework/src/fi/pyramus/framework/PyramusViewController.java
@@ -4,12 +4,52 @@ import fi.internetix.smvc.AccessDeniedException;
 import fi.internetix.smvc.LoginRequiredException;
 import fi.internetix.smvc.controllers.PageController;
 import fi.internetix.smvc.controllers.RequestContext;
+import fi.pyramus.dao.DAOFactory;
+import fi.pyramus.dao.users.UserDAO;
+import fi.pyramus.domainmodel.users.Role;
+import fi.pyramus.domainmodel.users.User;
 
 public abstract class PyramusViewController implements PageController {
 
+  public abstract UserRole[] getAllowedRoles();
+  
   public void authorize(RequestContext requestContext) throws LoginRequiredException, AccessDeniedException {
-    throw new RuntimeException("Not implemented");
+    UserRole[] roles = getAllowedRoles();
+    if (!contains(roles, UserRole.EVERYONE)) {
+      if (!requestContext.isLoggedIn())
+        throw new LoginRequiredException();
+      else {
+        Long loggedUserId = requestContext.getLoggedUserId();
+        
+        UserDAO userDAO = DAOFactory.getInstance().getUserDAO();
+        User user = userDAO.findById(loggedUserId);
+        
+        Role role = user.getRole();
+        
+        // TODO Ugly hax
+        UserRole userRole = UserRole.getRole(role.getValue());
+        
+        if (!contains(roles, userRole))
+          throw new AccessDeniedException(requestContext.getRequest().getLocale());
+      }
+    }
   }
 
-  public abstract UserRole[] getAllowedRoles();
+  /**
+   * Returns whether the given role is included in the given role array.
+   * 
+   * @param roles The roles
+   * @param role The role
+   * 
+   * @return <code>true</code> if the roles array contains the given role, otherwise
+   * <code>false</code>
+   */
+  private boolean contains(UserRole[] roles, UserRole role) {
+    for (int i = 0; i < roles.length; i++) {
+      if (roles[i] == role) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
