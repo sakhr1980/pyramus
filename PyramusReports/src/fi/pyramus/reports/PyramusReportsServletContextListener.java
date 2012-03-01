1828738707ce71f6a294685972bca35502c1d8db
ainen <antti.viljakainen@otavanopisto.fi>
2012-03-01 09:04:34 +0000

    - Added trustSelfSignedCerts methods

--- PyramusReports/src/fi/pyramus/reports/PyramusReportsServletContextListener.java
+++ PyramusReports/src/fi/pyramus/reports/PyramusReportsServletContextListener.java
@@ -1,5 +1,9 @@
 package fi.pyramus.reports;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -26,5 +30,29 @@ public class PyramusReportsServletContextListener implements ServletContextListe
   public void contextInitialized(ServletContextEvent servletContextEvent) {
     ServletContext ctx = servletContextEvent.getServletContext();
     ctx.getSessionCookieConfig().setName("JSESSIONID_PYRAMUSREPORTS");
+    
+    trustSelfSignedCerts();
   }
+  
+  private static void trustSelfSignedCerts() {
+    try {
+      TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+          return null;
+        }
+  
+        public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+        }
+  
+        public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+        }
+      } };
+  
+      SSLContext sc = SSLContext.getInstance("SSL");
+      sc.init(null, trustAllCerts, new java.security.SecureRandom());
+      HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    } catch (Exception e) {
+    }
+  }
+  
 }
