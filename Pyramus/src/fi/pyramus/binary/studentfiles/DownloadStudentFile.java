80d5f88a0f2979808550c7c39293e1995cbfc128
antti.leppa <antti.leppa@otavanopisto.fi>
2012-06-04 10:15:44 +0000

    ** Refactored DownloadStudentFile binary controller to setFileName and setResponseContent methods for outputting file data ** Fixed some inconsistencies from locale files

--- Pyramus/src/fi/pyramus/binary/studentfiles/DownloadStudentFile.java
+++ Pyramus/src/fi/pyramus/binary/studentfiles/DownloadStudentFile.java
@@ -1,18 +1,9 @@
 package fi.pyramus.binary.studentfiles;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import javax.servlet.ServletOutputStream;
-
 import fi.internetix.smvc.controllers.BinaryRequestContext;
 import fi.pyramus.dao.DAOFactory;
 import fi.pyramus.dao.file.StudentFileDAO;
-import fi.pyramus.dao.students.StudentDAO;
-import fi.pyramus.dao.students.StudentImageDAO;
 import fi.pyramus.domainmodel.file.StudentFile;
-import fi.pyramus.domainmodel.students.Student;
-import fi.pyramus.domainmodel.students.StudentImage;
 import fi.pyramus.framework.BinaryRequestController;
 import fi.pyramus.framework.UserRole;
 
@@ -26,17 +17,8 @@ public class DownloadStudentFile extends BinaryRequestController {
     StudentFile studentFile = studentFileDAO.findById(fileId);
     
     if (studentFile != null) {
-      binaryRequestContext.getResponse().setHeader("Content-Disposition", "inline; filename=\"" + studentFile.getFileName() + "\"");
-      binaryRequestContext.getResponse().setContentType(studentFile.getContentType());
-  
-      try {
-        ServletOutputStream outputStream = binaryRequestContext.getResponse().getOutputStream();
-        
-        outputStream.write(studentFile.getData());
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
+      binaryRequestContext.setFileName(studentFile.getFileName());
+      binaryRequestContext.setResponseContent(studentFile.getData(), studentFile.getContentType());
     }
   }
   
