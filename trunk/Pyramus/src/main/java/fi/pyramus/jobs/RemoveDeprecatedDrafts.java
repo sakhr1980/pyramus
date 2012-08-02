dee1dea2b933f8987dd4862d8b91cb63400e1f7d
ilmo.euro@otavanopisto.fi <ilmo.euro@otavanopisto.fi@1c9ff82f-59b1-6a62-74d4-2be32bec2a44>
2012-08-02 10:14:10 +0000

    ** Added Javadoc

--- trunk/Pyramus/src/main/java/fi/pyramus/jobs/RemoveDeprecatedDrafts.java
+++ trunk/Pyramus/src/main/java/fi/pyramus/jobs/RemoveDeprecatedDrafts.java
@@ -6,9 +6,15 @@ import javax.ejb.Stateless;
 import fi.pyramus.dao.DAOFactory;
 import fi.pyramus.dao.drafts.DraftDAO;
 
+/** A background job that removes deprecated drafts
+ *  from the draft repository periodically.
+ */
 @Stateless
 public class RemoveDeprecatedDrafts {
 
+  /** Removes the deprecated drafts from the repository.
+   * This method is called automatically every 5 minutes.
+   */
   @Schedule(second = "0", minute = "0/5", hour = "*", persistent = false)
   public void removeDeprecatedDrafts(){
     DraftDAO draftDAO = DAOFactory.getInstance().getDraftDAO();
