1eae5e5c0b8a4b7414afa97ffcba0654bfef0cfb
ilmo.euro <ilmo.euro@otavanopisto.fi>
2012-08-02 10:15:18 +0000

    ** Added Javadoc

--- PyramusSimplePlugin/src/main/java/fi/pyramus/plugin/simple/SimpleDomainModelExtensionDescriptor.java
+++ PyramusSimplePlugin/src/main/java/fi/pyramus/plugin/simple/SimpleDomainModelExtensionDescriptor.java
@@ -6,6 +6,9 @@ import java.util.Set;
 import fi.pyramus.domainmodel.DomainModelExtensionDescriptor;
 import fi.pyramus.plugin.simple.domainmodel.users.SimpleAuth;
 
+/** The descriptor that exposes the plugin's domain model extensions to host program.
+ *
+ */
 public class SimpleDomainModelExtensionDescriptor implements DomainModelExtensionDescriptor {
 
   public Set<Class<?>> getEntityClasses() {
--- PyramusSimplePlugin/src/main/java/fi/pyramus/plugin/simple/auth/SimpleAuthenticationProvider.java
+++ PyramusSimplePlugin/src/main/java/fi/pyramus/plugin/simple/auth/SimpleAuthenticationProvider.java
@@ -7,13 +7,26 @@ import fi.pyramus.plugin.auth.InternalAuthenticationProvider;
 import fi.pyramus.plugin.simple.dao.SimpleAuthDAO;
 import fi.pyramus.plugin.simple.domainmodel.users.SimpleAuth;
 
+/** A simple user/password authentication provider. */
 @SuppressWarnings("unused")
 public class SimpleAuthenticationProvider implements InternalAuthenticationProvider {
 
+  /** Returns the name of this authentication provider.
+   * 
+   * @return The name of this authentication provider.
+   */
   public String getName() {
     return "simple";
   }
 
+  /** Returns the user object corresponding to the specified
+   * username and password.
+   * 
+   * @param username The username of the user to retrieve.
+   * @param password The password of the user to retrieve.
+   * @return The user with the specified username and password, or <code>null</code>
+   * if the username and/or password is incorrect.
+   */
   public User getUser(String username, String password) {
     UserDAO userDAO = DAOFactory.getInstance().getUserDAO();
     SimpleAuthDAO simpleAuthDAO = new SimpleAuthDAO();
@@ -28,6 +41,11 @@ public class SimpleAuthenticationProvider implements InternalAuthenticationProvi
     }
   }
   
+  /** Returns the username of the user with the specified external ID.
+   * 
+   * @param externalId The external ID of the user to retrieve.
+   * @return The user whose external ID is <code>externalId</code>
+   */
   public String getUsername(String externalId) {
     SimpleAuthDAO simpleAuthDAO = new SimpleAuthDAO();
     UserDAO userDAO = DAOFactory.getInstance().getUserDAO();
@@ -40,10 +58,19 @@ public class SimpleAuthenticationProvider implements InternalAuthenticationProvi
     return null;
   }
 
+  /** Returns <code>true</code> if user credentials can be updated.
+   *  @return <code>true</code> if user credentials can be updated.
+   */
   public boolean canUpdateCredentials() {
     return true;
   }
   
+  /** Add a new user with specified username and password.
+   * 
+   * @param username The username of the new user.
+   * @param password The password of the new user.
+   * @return The external ID of the new user.
+   */
   public String createCredentials(String username, String password) {
     SimpleAuthDAO simpleAuthDAO = new SimpleAuthDAO();
     UserDAO userDAO = DAOFactory.getInstance().getUserDAO();
@@ -55,6 +82,11 @@ public class SimpleAuthenticationProvider implements InternalAuthenticationProvi
     return externalId;
   }
   
+  /** Changes a user's password to <code>password</code>.
+   * 
+   * @param externalId The external ID of the user to modify.
+   * @param password The new password for the user.
+   */
   public void updatePassword(String externalId, String password) {
     SimpleAuthDAO simpleAuthDAO = new SimpleAuthDAO();
     
@@ -63,6 +95,11 @@ public class SimpleAuthenticationProvider implements InternalAuthenticationProvi
     simpleAuthDAO.updatePassword(simpleAuth, password);
   }
   
+  /** Changes a user's username to <code>username</code>.
+   * 
+   * @param externalId The external ID of the user to modify.
+   * @param username The new username for the user.
+   */
   public void updateUsername(String externalId, String username) {
     SimpleAuthDAO simpleAuthDAO = new SimpleAuthDAO();
     
