a4ffcd08c4945b2210a0bb3fa4486c36f18f72f6
ilmo.euro <ilmo.euro@otavanopisto.fi>
2012-08-02 10:14:10 +0000

    ** Added Javadoc

--- Pyramus/src/main/java/fi/pyramus/taglib/ExtensionHookTag.java
+++ Pyramus/src/main/java/fi/pyramus/taglib/ExtensionHookTag.java
@@ -12,10 +12,31 @@ import fi.pyramus.plugin.PageHookContext;
 import fi.pyramus.plugin.PageHookController;
 import fi.pyramus.plugin.PageHookVault;
 
+/** This class adds support for extension hooks in JSP pages.
+ * Extension hooks allow plugins to add their own content to
+ * already existing pages. Usage:
+ * <br/>
+ * <ul>
+ *   <li>Add an <code>ix:extensionhook</code> tag to the page you want extended. The tag
+ *   will be substituted with content provided by the plugin:<br/>
+ *   <code>&lt;!-- ... contents of the existing page --&gt;</code><br/>
+ *   <code>&lt;!-- The plugin-provided content will be added after this line --&gt;</code><br/>
+ *   <code>&lt;ix:extensionHook name="<i>the name of the hook</i>"/&gt;</code><br/>
+ *   <code>&lt;!-- ... contents of the existing page --&gt;</code><br/> 
+ *   </li>
+ *   <li>Add a subclass of <code>PageHookController</code> to the
+ *   plugin that extends an existing page.</li>
+ *   <li>Add an entry of that <code>PageHookController</code> to the
+ *   <code>getPageHookControllers</code> method of the
+ *   plugin's <code>PluginDescriptor</code>.</li>
+ * </ul>
+ */
 public class ExtensionHookTag extends TagSupport {
 
   private static final long serialVersionUID = 5603128082864676937L;
-
+  /** The content substitution is done here.
+   *  @returns SKIP_BODY
+   */
   public int doEndTag() throws javax.servlet.jsp.JspTagException {
     List<PageHookController> hookControllers = PageHookVault.getInstance().getPageHooks(getName());
     if (hookControllers != null) {
@@ -40,10 +61,18 @@ public class ExtensionHookTag extends TagSupport {
     return SKIP_BODY;
   }
 
+  /** Returns the name of the tag.
+   * 
+   * @return The name of the tag.
+   */
   public String getName() {
     return name;
   }
 
+  /** Sets the name of the tag.
+   * 
+   * @param name The new name of the tag
+   */
   public void setName(String name) {
     this.name = name;
   }
