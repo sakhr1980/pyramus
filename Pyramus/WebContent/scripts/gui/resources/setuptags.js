9ceecc4c2a2f691fd76546f828b31633ddfd5b22
ainen <antti.viljakainen@otavanopisto.fi>
2013-02-20 12:42:04 +0000

    - Version 0.6.4 Final

--- Pyramus/WebContent/scripts/gui/resources/setuptags.js
+++
@@ -1,9 +0,0 @@
-function setupTags() {
-  JSONRequest.request("tags/getalltags.json", {
-    onSuccess : function(jsonResponse) {
-      new Autocompleter.Local("tags", "tags_choices", jsonResponse.tags, {
-        tokens : [ ',', '\n', ' ' ]
-      });
-    }
-  });
-}
